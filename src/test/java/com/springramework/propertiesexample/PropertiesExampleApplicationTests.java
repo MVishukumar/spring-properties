93692ed1a88ff810976d41bf6b833ca5cb5e9ea0
MVishukumar <beingvishu@gmail.com>
2020-12-20 15:49:27 +0530

    initial commit

---
+++ src/test/java/com/springramework/propertiesexample/PropertiesExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springramework.propertiesexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertiesExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
