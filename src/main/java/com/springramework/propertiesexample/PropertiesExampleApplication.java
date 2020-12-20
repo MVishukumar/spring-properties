b505d8d83353d7e39f12ab5a6be5277488506d9f
MVishukumar <beingvishu@gmail.com>
2020-12-20 16:19:44 +0530

    Added multiple property sources

--- src/main/java/com/springramework/propertiesexample/PropertiesExampleApplication.java
+++ src/main/java/com/springramework/propertiesexample/PropertiesExampleApplication.java
@@ -1,6 +1,7 @@
 package com.springramework.propertiesexample;
 
 import com.springramework.propertiesexample.examplebeans.FakeDataSource;
+import com.springramework.propertiesexample.examplebeans.FakeJmsBroker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -15,6 +16,12 @@ public class PropertiesExampleApplication {
 		System.out.println(fakeDataSource.getUsername());
 		System.out.println(fakeDataSource.getPassword());
 		System.out.println(fakeDataSource.getDburl());
+
+
+		FakeJmsBroker fakeJmsBroker = (FakeJmsBroker) context.getBean("fakeJmsBroker");
+		System.out.println(fakeJmsBroker.getUsername());
+		System.out.println(fakeJmsBroker.getPassword());
+		System.out.println(fakeJmsBroker.getUrl());
 	}
 
 }
--- src/main/java/com/springramework/propertiesexample/config/PropertyConfig.java
+++ src/main/java/com/springramework/propertiesexample/config/PropertyConfig.java
@@ -1,17 +1,23 @@
 package com.springramework.propertiesexample.config;
 
 import com.springramework.propertiesexample.examplebeans.FakeDataSource;
+import com.springramework.propertiesexample.examplebeans.FakeJmsBroker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.EnvironmentAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.env.Environment;
 
 @Configuration
-@PropertySource("classpath:datasource.properties")
+//@PropertySource({"classpath:datasource.properties", "classpath:jms.properties"})
+@PropertySources({
+        @PropertySource("classpath:datasource.properties"),
+        @PropertySource("classpath:jms.properties"),
+})
 public class PropertyConfig {
 
     @Autowired
@@ -26,6 +32,15 @@ public class PropertyConfig {
     @Value("${spring.dburl}")
     String dburl;
 
+    @Value("${spring.jms.username}")
+    String jmsUsername;
+
+    @Value("${spring.jms.password}")
+    String jmsPassword;
+
+    @Value("${spring.jms.dburl}")
+    String jmsUrl;
+
     @Bean
     public FakeDataSource fakeDataSource() {
         FakeDataSource fakeDataSource = new FakeDataSource();
@@ -34,6 +49,16 @@ public class PropertyConfig {
         fakeDataSource.setDburl(dburl);
 
         return fakeDataSource;
+    }
+
+    @Bean
+    public FakeJmsBroker fakeJmsBroker() {
+        FakeJmsBroker fakeJmsBroker = new FakeJmsBroker();
+        fakeJmsBroker.setUsername(jmsUsername);
+        fakeJmsBroker.setPassword(jmsPassword);
+        fakeJmsBroker.setUrl(jmsUrl);
+
+        return fakeJmsBroker;
     }
 
     @Bean
